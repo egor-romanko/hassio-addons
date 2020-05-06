9757711181ed80cb94203b5ceecc414bee5e7811
EgorR <e.romanko@itransition.com>
2020-05-06 17:58:57 +0300

    Use base mopidy image; add icecast

--- mopidy-icecast2/run.sh
+++ mopidy-icecast2/run.sh
@@ -5,9 +5,14 @@ local_scan=$(cat /data/options.json | jq -r '.local_scan // empty')
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
 config="/var/lib/mopidy/.config/mopidy/mopidy.conf"
 
-icecast2 -c /etc/icecast2/icecast.xml
+export local_scan
+export options
+export config
 
-if  [ "$local_scan" == "true" ]; then
-    mopidy --config $config $options local scan
-fi
-mopidy --config $config $options
+mkdir -p /data/icecast2/logs
+touch /data/icecast2/logs/error.log
+touch /data/icecast2/logs/access.log
+chmod -R a+rwX /data/
+printenv
+
+/usr/bin/supervisord -c /etc/supervisord.conf
